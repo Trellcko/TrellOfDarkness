c8313614677ab28bcafc2ab70c73ccd2b7984072
AntonDobonii <antond@amayasoft.uz>
2021-07-11 16:23:33 +0300

    Add Hand Of Dark

---
+++ Assets/Scipts/Magic/DarkSpells/HandOfDark.cs
@@ -0,0 +1,14 @@
+﻿using TrellOfDarkness.Animation;
+using UnityEngine;
+
+namespace TrellOfDarkness.Magic.DarkSpell
+{
+    public class HandOfDark : MonoBehaviour, IDarkSpell
+    {
+        [SerializeField] private CastingAnimation _animator;
+        public void Cast()
+        {
+            _animator.PlayCast();
+        }
+    }
+}
