e3c6609e92dbc5bfcbd442ac20e624d4b5ddaa59
AntonDobonii <antond@amayasoft.uz>
2021-07-11 16:21:56 +0300

    Add CastingAnimation for BaseCastingAnimator

---
+++ Assets/Scipts/Animation/CastingAnimation.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace TrellOfDarkness.Animation
+{
+    public class CastingAnimation : MonoBehaviour
+    {
+        private Animator _animator;
+
+        private const string Cast = "Cast";
+
+        public void PlayCast()
+        {
+            _animator.SetTrigger(Cast);
+        }
+    }
+}
