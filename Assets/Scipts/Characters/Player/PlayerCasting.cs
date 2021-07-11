7a7fa7b9873a61d1fece5039e069b2aa8950fda0
AntonDobonii <antond@amayasoft.uz>
2021-07-11 16:22:20 +0300

    Add PlaterCasting

---
+++ Assets/Scipts/Characters/Player/PlayerCasting.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+using TrellOfDarkness.Magic.DarkSpell;
+using System.Linq;
+
+namespace TrellOfDarkness.Characters.Player
+{
+    public class PlayerCasting : MonoBehaviour
+    {
+        [SerializeField] private GameObject _darkSpellsParent;
+
+        private List<IDarkSpell> _darkSpells = new List<IDarkSpell>();
+
+        private void Awake()
+        {
+            _darkSpells = _darkSpellsParent.GetComponentsInChildren<IDarkSpell>().ToList();
+        }
+
+    }
+}
